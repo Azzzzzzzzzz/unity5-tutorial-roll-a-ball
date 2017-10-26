9eda3f8a65d3bf7cdfaf4231c3903754744a4e8b
Azzzzzzzzzz <31974708+Azzzzzzzzzz@users.noreply.github.com>
2017-10-27 09:55:14 +1000

    Added fixed camera to player object
    Camera now remains fixed to player object, following the player object around the scene

---
+++ 01_RollABall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        /* transform.position is the position in the scene where the attached object is
+           We calculate the difference in position as an offset, then when each frame 
+           is updated we change the camera position so it remains relative to the 
+           player object
+        */
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
